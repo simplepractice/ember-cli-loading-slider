f64e8a0799dc62530601b6135c40255c87031540
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2024-02-26 18:36:35 +0200

    Fix route event

--- app/mixins/loading-slider.js
+++ app/mixins/loading-slider.js
@@ -1,19 +1,17 @@
 import { inject as service } from '@ember/service';
 import Mixin from '@ember/object/mixin';
-import { isPresent } from '@ember/utils';
 
 export default Mixin.create({
   loadingSlider: service(),
+  router: service(),
 
   actions: {
     loading() {
       let loadingSliderService = this.get('loadingSlider');
       loadingSliderService.startLoading();
-      if (isPresent(this._router)) {
-        this._router.one('didTransition', function() {
-          loadingSliderService.endLoading();
-        });
-      }
+      this.router.one('routeDidChange', function() {
+        loadingSliderService.endLoading();
+      });
       if (this.get('bubbleLoadingSlider')) {
         return true;
       }
